ba8f7822129dc108c37cdeae2bf354330dd89de4
Nguyen Le <nguyenle@Nguyens-MacBook-Pro.local>
2019-05-23 18:05:22 -0700

    UPdate

--- Callback_HigherOrderFunction/00codeburst_map_reduce_filter/object.js
+++ Callback_HigherOrderFunction/00codeburst_map_reduce_filter/object.js
@@ -42,18 +42,71 @@ Object.keys(obj).forEach(function (key) {
 
 //////////////////////////////////////////////////////////////////
 // Solution 01
-Object.keys(obj).reduce((p,c)=> p.concat(obj[c]),[]) ;
+Object.keys(obj).reduce((p, c) => p.concat(obj[c]), []);
 
 // Solution 02
 mapr = o => Object.keys(o).map(key => o[key]);
 
 const tmp = mapr(validation_messages);
 
-for(let t in tmp){
+for (let t in tmp) {
     console.log(tmp[t]);
 }
 
+// Solution 03: If you are in react, you probably want to use .map() instead of .forEach()
+// https://stackoverflow.com/questions/39965579/how-to-loop-an-object-in-react
 
 /////////////////////////////////////////////////////////////////
+var myObj = {
+    a: 'foo',
+    b: 'bar'
+};
 
-//console.log(mapr);
+// In ES6 you can loop through an object like this:
+Object.keys(myObj).forEach(key => {
+    console.log(key);          // the name of the current key.
+    console.log(myObj[key]);   // the value of the current key.
+});
+
+// The above would also work as a one-liner:
+Object.keys(myObj).forEach(key => console.log(key, myObj[key]));
+
+// In ES7 you can use Object.entries instead of Object.keys and loop through an object like this:
+Object.entries(myObj).forEach(([key, val]) => {
+    console.log(key);          // the name of the current key.
+    console.log(val);          // the value of the current key.
+});
+
+/////////////////////////////////////
+
+// In case you want to loop through nested objects as well, you can use a recursive function (ES6):
+const loopNestedObj = (obj) => {
+    Object.keys(obj).forEach(key => {
+        if (obj[key] && typeof obj[key] === 'object') loopNestedObj(obj[key]);  // recurse.
+        else console.log(key, obj[key]);  // or do something with key and val.
+    });
+};
+
+// const tmp1 = loopNestedObj(validation_messages);
+
+// Same as function above, but with ES7 Object.entries instead of Object.keys:
+const loopNestedObj = (obj) => {
+    Object.entries(obj).forEach(([key, val]) => {
+        if (val && typeof val === 'object') loopNestedObj(val);  // recurse.
+        else console.log(key, val);  // or do something with key and val.
+    });
+};
+
+//////////////////////////////////
+// If you are into functional programming you can use Object.keys/Object.entries to enumerate the object, then process the values and then use reduce() to convert back to a new object.
+const loopNestedObj = (obj) =>
+    Object.keys(obj)
+        // Use .filter(), .map(), etc. if you need.
+        .reduce((newObj, key) =>
+            (obj[key] && typeof obj[key] === 'object') ?
+                { ...newObj, [key]: loopNestedObj(obj[key]) } :  // recurse.
+                { ...newObj, [key]: obj[key] },                  // Define value.
+            {});
+/////////////////////////////////////////////////////////////////
+
+
